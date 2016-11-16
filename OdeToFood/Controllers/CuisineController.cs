5d2dae1ac4829f5a3b1ee2dc822a4115b8926f06
shevchenkotaras <shevchenkotv91@gmail.com>
2016-11-16 22:21:35 +0200

    FirstCommit

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,24 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        
+        [Log]
+        public ActionResult Search(string name = "french")
+        {
+            throw new Exception("Something terrible has happend");
+
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+    }
+}
