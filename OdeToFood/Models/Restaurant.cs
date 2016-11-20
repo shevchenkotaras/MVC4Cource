ba3151423455ff538fa590dd30aea9016c4bafc6
shevchenkotaras <shevchenkotv91@gmail.com>
2016-11-20 16:26:41 +0200

    Test development branch

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -7,6 +7,7 @@ namespace OdeToFood.Models
 {
     public class Restaurant
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
