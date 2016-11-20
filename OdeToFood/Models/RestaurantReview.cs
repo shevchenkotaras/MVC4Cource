71d967e13e71551e9d607b4048bc4d4934bfb21f
shevchenkotaras <shevchenkotv91@gmail.com>
2016-11-20 20:19:00 +0200

    Custom validations

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,7 +6,31 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class RestaurantReview
+    //public class MaxWordsAttribute : ValidationAttribute
+    //{
+    //    public MaxWordsAttribute(int maxWords) : base("{0} has to many words.")
+    //    {
+    //        _maxWords = maxWords;
+    //    }
+
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        if (validationContext != null)
+    //        {
+    //            var valueAsString = value.ToString();
+    //            if (valueAsString.Split(' ').Length > _maxWords) 
+    //            {
+    //                var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+    //                return new ValidationResult(errorMessage);
+    //            }
+    //        }
+    //        return ValidationResult.Success; 
+    //    }
+
+    //    private readonly int _maxWords;
+    //}
+
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }     
         [Range(1,10)]   
@@ -15,8 +39,16 @@ namespace OdeToFood.Models
         [StringLength(1024)]
         public string Body { get; set; }
         [Display(Name ="User name")]
-        [DisplayFormat(NullDisplayText ="anonynous")]
+        [DisplayFormat(NullDisplayText ="anonynous")]        
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2)
+            {
+                yield return new ValidationResult("Sorry, you cant do this");
+            }
+        }
     }
 }
