cad10a5797b74724638595409192c3fd3579b5fb
shevchenkotaras <shevchenkotv91@gmail.com>
2016-11-20 17:22:04 +0200

    Editing a Review

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -21,6 +21,24 @@ namespace OdeToFood.Controllers
             return HttpNotFound();
         }
 
+        [HttpGet]
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
+
         protected override void Dispose(bool disposing)
         {
             _db.Dispose();
